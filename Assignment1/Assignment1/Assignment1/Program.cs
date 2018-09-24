62757f7ddc7edd66efaa34d6bd39717b25b4f738
raymondshiner <37393100+raymondshiner@users.noreply.github.com>
2018-09-24 09:38:33 -0700

    My Initial Assignment 1 Commit - basic code for assingment

---
+++ Assignment1/Assignment1/Assignment1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter a String to be Repeated: ");
+
+            string value = Console.ReadLine();
+
+            Console.WriteLine("Your String is " + value);
+            Console.WriteLine("Please press any key to exit . . .");
+
+            Console.ReadLine();
+        }
+    }
+}
