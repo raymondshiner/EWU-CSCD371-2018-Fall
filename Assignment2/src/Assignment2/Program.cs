22aa07f8c1104a534290dd6d383f070ab9cc1484
Raymond Shiner <raymondshiner@gmail.com>
2018-09-27 22:41:50 -0700

    Git init - added .gitignore file and Set up Assignment 2 hierarchy

---
+++ Assignment2/src/Assignment2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Assignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myInt = 42;
+            System.Int32 myOtherInt = 42;
+            WriteLine("Raymond");
+        }
+    }
+}
