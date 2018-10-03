15ff3fb224bb210862e4d530188d7a9ee7d9b178
raymondshiner <37393100+raymondshiner@users.noreply.github.com>
2018-10-03 00:19:50 -0700

    Program done, Unit Tests done with Add, Sub, Mult - 10/3 12:19am - Desktop

--- Assignment2/src/Assignment2/Program.cs
+++ Assignment2/src/Assignment2/Program.cs
@@ -2,7 +2,7 @@
 
 namespace Assignment2
 {
-    class Program
+    public class Program
     {
         public static void Main(string[] args)
         {
@@ -12,7 +12,9 @@ namespace Assignment2
 
             string[] array;
 
-            if (input.Contains('+'))
+
+
+            if (input.Contains('+')) //addition
             {
                 array = input.Split('+');
 
@@ -24,19 +26,10 @@ namespace Assignment2
                 Console.WriteLine("{0}+{1} = {2}", num1, num2, res);
             }
 
-            else if (input.Contains('-'))
-            {
-                array = input.Split('-');
 
-                int.TryParse(array[0], out int num1);
-                int.TryParse(array[1], out int num2);
 
-                int res = num1 - num2;
 
-                Console.WriteLine("{0}-{1} = {2}", num1, num2, res);
-            }
-
-            else if (input.Contains('*'))
+            else if (input.Contains('*')) //multiplication
             {
                 array = input.Split('*');
 
@@ -48,7 +41,10 @@ namespace Assignment2
                 Console.WriteLine("{0}*{1} = {2}", num1, num2, res);
             }
 
-            else if (input.Contains('/'))
+
+
+
+            else if (input.Contains('/')) //division
             {
                 array = input.Split('/');
 
@@ -68,10 +64,72 @@ namespace Assignment2
                 }
             }
 
-            else
+
+
+
+            else if (input.Contains('-')) //subtraction - by checking for subtraction last it ensures that negatives work properly for all above calculations
+            {
+                array = input.Split('-');
+
+                int num1, num2;
+
+                if (array.Length == 2) // no negatives
+                {
+                    int.TryParse(array[0], out num1);
+                    int.TryParse(array[1], out num2);
+                }
+
+                else if (array.Length == 3) // 1 negative
+                {
+                    if (array[0].Equals(""))        //if array[0] is empty string then first num is negative - second num positive
+                    {                               //this happens because of way that split("-") works: "-2-3" --> { "", "2", "3" }
+
+                        int.TryParse(array[1], out num1);
+                        int.TryParse(array[2], out num2);
+
+                        num1 = num1 * (-1);         //make num1 negative
+                    }
+
+                    else                            // second num negative - first number postiive - this splits as such: "2--3" --> { "2", "", "3" }
+                    {
+                        int.TryParse(array[0], out num1);
+                        int.TryParse(array[2], out num2);
+
+                        num2 = num2 * (-1);         //make num2 negative
+                    }
+                }
+
+                else if (array.Length == 4) // both negative - splits as such: "-2--3" --> { "", "2", "", "3" }
+                {
+                    int.TryParse(array[1], out num1);
+                    int.TryParse(array[3], out num2);
+
+                    num1 = num1 * (-1);
+                    num2 = num2 * (-1);         //make both nums negative
+                }
+
+                else // user puts in more than 2 negative symbols
+                {
+                    Console.WriteLine("This program does not handle double negatives on numbers - returning 0");
+                    num1 = 0;
+                    num2 = 0;
+                }
+
+                int res = num1 - num2;
+
+                Console.WriteLine("{0}-{1} = {2}", num1, num2, res);
+            }
+
+
+
+
+            else //user doesnt input a math Symbol
             {
                 Console.WriteLine("ERROR - Please include an operator (+, -, *, /) in your input");
             }
+
+
+
 
             Console.Write("Press Enter to Exit Program . . .");
 
--- Assignment2/test/Assignment2.Tests/UnitTest1.cs
+++ Assignment2/test/Assignment2.Tests/UnitTest1.cs
@@ -3,12 +3,341 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Assignment2.Tests
 {
     [TestClass]
-    public class UnitTest1
+    public class TestAddition
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestAdd8And2()
         {
-            
+            string expectedOuput = $@">>Please enter a calculation to perform: <<8+2
+>>8+2 = 10
+>>Press Enter to Exit Program . . .";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(expectedOuput, Assignment2.Program.Main);
+        }
+
+        [TestMethod]
+        public void TestAdd6And90()
+        {
+            string expectedOuput = $@">>Please enter a calculation to perform: <<6+90
+>>6+90 = 96
+>>Press Enter to Exit Program . . .";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(expectedOuput, Assignment2.Program.Main);
+        }
+
+        [TestMethod]
+        public void TestAddMinAnd5()
+        {
+            int minVal = int.MinValue;
+            int five = 5;
+            int res = minVal + five;
+
+            string expectedOuput = $@">>Please enter a calculation to perform: <<{minVal}+{five}
+>>{minVal}+{five} = {res}
+>>Press Enter to Exit Program . . .";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(expectedOuput, Assignment2.Program.Main);
+        }
+
+        [TestMethod]
+        public void TestAddMaxAndNeg7()
+        {
+            int maxVal = int.MaxValue;
+            int negSeven = -7;
+            int res = maxVal + negSeven;
+
+            string expectedOuput = $@">>Please enter a calculation to perform: <<{maxVal}+{negSeven}
+>>{maxVal}+{negSeven} = {res}
+>>Press Enter to Exit Program . . .";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(expectedOuput, Assignment2.Program.Main);
+        }
+
+        [TestMethod]
+        public void TestAdd0AndNeg7()
+        {
+            int zero = 0;
+            int negSeven = -7;
+            int res = zero + negSeven;
+
+            string expectedOuput = $@">>Please enter a calculation to perform: <<{zero}+{negSeven}
+>>{zero}+{negSeven} = {res}
+>>Press Enter to Exit Program . . .";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(expectedOuput, Assignment2.Program.Main);
         }
     }
+    
+    //END OF ADDITION TESTS
+    
+
+
+
+
+
+
+    //START OF SUBTRACTION TESTS
+
+    [TestClass]
+    public class TestSubtraction
+    {
+        [TestMethod]
+        public void TestSub8And2HardCoded()
+        {
+            string expectedOuput = $@">>Please enter a calculation to perform: <<8-2
+>>8-2 = 6
+>>Press Enter to Exit Program . . .";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(expectedOuput, Assignment2.Program.Main);
+        }
+
+        [TestMethod]
+        public void TestSub297And43()
+        {
+            int num1 = 297;
+            int num2 = 43;
+            int res = num1 - num2;
+
+            string expectedOuput = $@">>Please enter a calculation to perform: <<{num1}-{num2}
+>>{num1}-{num2} = {res}
+>>Press Enter to Exit Program . . .";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(expectedOuput, Assignment2.Program.Main);
+        }
+
+        [TestMethod]
+        public void TestSubNeg4And16()
+        {
+            int num1 = -4;
+            int num2 = 16;
+            int res = num1 - num2;
+
+            string expectedOuput = $@">>Please enter a calculation to perform: <<{num1}-{num2}
+>>{num1}-{num2} = {res}
+>>Press Enter to Exit Program . . .";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(expectedOuput, Assignment2.Program.Main);
+        }
+
+        [TestMethod]
+        public void TestSub5AndNeg12()
+        {
+            int num1 = 5;
+            int num2 = -12;
+            int res = num1 - num2;
+
+            string expectedOuput = $@">>Please enter a calculation to perform: <<{num1}-{num2}
+>>{num1}-{num2} = {res}
+>>Press Enter to Exit Program . . .";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(expectedOuput, Assignment2.Program.Main);
+        }
+
+        [TestMethod]
+        public void TestSubNeg2AndNeg4()
+        {
+            int num1 = -2;
+            int num2 = -4;
+            int res = num1 - num2;
+
+            string expectedOuput = $@">>Please enter a calculation to perform: <<{num1}-{num2}
+>>{num1}-{num2} = {res}
+>>Press Enter to Exit Program . . .";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(expectedOuput, Assignment2.Program.Main);
+        }
+        /*
+        [TestMethod]
+        public void TestSubNeg2AndMinVal()
+        {
+            int num1 = -2;
+            int num2 = int.MinValue;
+            int res = num1 - num2;
+
+            string expectedOuput = $@">>Please enter a calculation to perform: <<{num1}-{num2}
+>>{num1}-{num2} = {res}
+>>Press Enter to Exit Program . . .";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(expectedOuput, Assignment2.Program.Main);
+        }
+        */
+
+        [TestMethod]
+        public void TestSubNeg2AndMaxVal()
+        {
+            int num1 = -2;
+            int num2 = int.MaxValue;
+            int res = num1 - num2;
+
+            string expectedOuput = $@">>Please enter a calculation to perform: <<{num1}-{num2}
+>>{num1}-{num2} = {res}
+>>Press Enter to Exit Program . . .";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(expectedOuput, Assignment2.Program.Main);
+        }
+
+        [TestMethod]
+        public void TestSubMaxValAnd4()
+        {
+            int num1 = int.MaxValue;
+            int num2 = 4;
+            int res = num1 - num2;
+
+            string expectedOuput = $@">>Please enter a calculation to perform: <<{num1}-{num2}
+>>{num1}-{num2} = {res}
+>>Press Enter to Exit Program . . .";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(expectedOuput, Assignment2.Program.Main);
+        }
+
+        [TestMethod]
+        public void TestSub0And4()
+        {
+            int num1 = 0;
+            int num2 = 4;
+            int res = num1 - num2;
+
+            string expectedOuput = $@">>Please enter a calculation to perform: <<{num1}-{num2}
+>>{num1}-{num2} = {res}
+>>Press Enter to Exit Program . . .";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(expectedOuput, Assignment2.Program.Main);
+        }
+
+        [TestMethod]
+        public void TestSub0And7HardCoded()
+        { 
+            string expectedOuput = $@">>Please enter a calculation to perform: <<0-7
+>>0-7 = -7
+>>Press Enter to Exit Program . . .";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(expectedOuput, Assignment2.Program.Main);
+        }
+    }
+
+    //END OF SUBTRACTION TESTS
+
+
+
+
+
+
+
+
+    //START OF MULTIPLICATION TESTS
+
+    [TestClass]
+
+    public class TestMultiplication
+    {
+        [TestMethod]
+        public void TestMult4And7HardCoded()
+        {
+            string expectedOuput = $@">>Please enter a calculation to perform: <<4*7
+>>4*7 = 28
+>>Press Enter to Exit Program . . .";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(expectedOuput, Assignment2.Program.Main);
+        }
+
+        [TestMethod]
+        public void TestMult0And15HardCoded()
+        {
+            int num1 = -2;
+            int num2 = 40;
+            int res = num1 * num2;
+
+            string expectedOuput = $@">>Please enter a calculation to perform: <<0*15
+>>0*15 = 0
+>>Press Enter to Exit Program . . .";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(expectedOuput, Assignment2.Program.Main);
+        }
+
+        [TestMethod]
+        public void TestMultNeg2And40()
+        {
+            int num1 = -2;
+            int num2 = 40;
+            int res = num1 * num2;
+
+            string expectedOuput = $@">>Please enter a calculation to perform: <<{num1}*{num2}
+>>{num1}*{num2} = {res}
+>>Press Enter to Exit Program . . .";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(expectedOuput, Assignment2.Program.Main);
+        }
+
+        [TestMethod]
+        public void TestMult8AndNeg20()
+        {
+            int num1 = 8;
+            int num2 = 20;
+            int res = num1 * num2;
+
+            string expectedOuput = $@">>Please enter a calculation to perform: <<{num1}*{num2}
+>>{num1}*{num2} = {res}
+>>Press Enter to Exit Program . . .";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(expectedOuput, Assignment2.Program.Main);
+        }
+
+        [TestMethod]
+        public void TestMultNeg13AndNeg4()
+        {
+            int num1 = -13;
+            int num2 = -4;
+            int res = num1 * num2;
+
+            string expectedOuput = $@">>Please enter a calculation to perform: <<{num1}*{num2}
+>>{num1}*{num2} = {res}
+>>Press Enter to Exit Program . . .";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(expectedOuput, Assignment2.Program.Main);
+        }
+
+        [TestMethod]
+        public void TestMultMinValAnd5()
+        {
+            int num1 = int.MinValue;
+            int num2 = 5;
+            int res = num1 * num2;
+
+            string expectedOuput = $@">>Please enter a calculation to perform: <<{num1}*{num2}
+>>{num1}*{num2} = {res}
+>>Press Enter to Exit Program . . .";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(expectedOuput, Assignment2.Program.Main);
+        }
+
+        [TestMethod]
+        public void TestMultMaxValAnd7()
+        {
+            int num1 = int.MaxValue;
+            int num2 = 7;
+            int res = num1 * num2;
+
+            string expectedOuput = $@">>Please enter a calculation to perform: <<{num1}*{num2}
+>>{num1}*{num2} = {res}
+>>Press Enter to Exit Program . . .";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(expectedOuput, Assignment2.Program.Main);
+        }
+
+        [TestMethod]
+        public void TestMultMaxValAndZero()
+        {
+            int num1 = int.MaxValue;
+            int num2 = 0;
+            int res = num1 * num2;
+
+            string expectedOuput = $@">>Please enter a calculation to perform: <<{num1}*{num2}
+>>{num1}*{num2} = {res}
+>>Press Enter to Exit Program . . .";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(expectedOuput, Assignment2.Program.Main);
+        }
+    }
+
+    //END OF MULTIPLICATION TESTS
 }
