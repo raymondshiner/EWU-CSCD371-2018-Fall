6e311756a3b61d49ab21349691c0f38dd8c8ea38
Raymond Shiner <raymondshiner@gmail.com>
2018-10-02 19:07:04 -0700

    Worked on Assignment 2 - 10/2 7:06pm - Laptop

--- Assignment2/src/Assignment2/Program.cs
+++ Assignment2/src/Assignment2/Program.cs
@@ -6,13 +6,76 @@ namespace Assignment2
     {
         public static void Main(string[] args)
         {
-            var result = 1 / 0;
+            Console.Write("Please enter a calculation to perform: ");
 
+            var input = Console.ReadLine();
 
-            Console.WriteLine("Raymond");
+            string[] array;
 
-            Console.Write("Press enter to Continue . . .");
-            Console.Read();
+            if (input.Contains('+'))
+            {
+                array = input.Split('+');
+
+                int.TryParse(array[0], out int num1);
+                int.TryParse(array[1], out int num2);
+
+                int res = num1 + num2;
+
+                Console.WriteLine("{0}+{1} = {2}", num1, num2, res);
+            }
+
+            else if (input.Contains('-'))
+            {
+                array = input.Split('-');
+
+                int.TryParse(array[0], out int num1);
+                int.TryParse(array[1], out int num2);
+
+                int res = num1 - num2;
+
+                Console.WriteLine("{0}-{1} = {2}", num1, num2, res);
+            }
+
+            else if (input.Contains('*'))
+            {
+                array = input.Split('*');
+
+                int.TryParse(array[0], out int num1);
+                int.TryParse(array[1], out int num2);
+
+                int res = num1 * num2;
+
+                Console.WriteLine("{0}*{1} = {2}", num1, num2, res);
+            }
+
+            else if (input.Contains('/'))
+            {
+                array = input.Split('/');
+
+                int.TryParse(array[0], out int num1);
+                int.TryParse(array[1], out int num2);
+
+                if (num2 != 0)
+                {
+                    int res = num1 / num2;
+
+                    Console.WriteLine("{0}/{1} = {2}", num1, num2, res);
+                }
+
+                else
+                {
+                    Console.WriteLine("Cannot Divide by Zero - Please Try Again");
+                }
+            }
+
+            else
+            {
+                Console.WriteLine("ERROR - Please include an operator (+, -, *, /) in your input");
+            }
+
+            Console.Write("Press Enter to Exit Program . . .");
+
+            Console.ReadLine();
         }
         
     }
