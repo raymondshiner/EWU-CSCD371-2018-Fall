26483bf3fa193e25c828c96d59fc99b71095c44b
Raymond Shiner <raymondshiner@gmail.com>
2018-10-09 23:10:32 -0700

    done with code of Assignment3, need tests - 410pm 10/9 - Laptop

--- Assignment3/src/Assignment3/Program.cs
+++ Assignment3/src/Assignment3/Program.cs
@@ -6,7 +6,41 @@ namespace Assignment3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool goAgain;
+
+            do
+            {
+                Console.WriteLine("Welcome to a game of Rock Paper Scissors");
+                Console.WriteLine("Both Players start with 100 life, its you versus the Computer");
+                Console.WriteLine("Good Luck - Have Fun");
+                Console.WriteLine();
+
+                int cpuLife = 100;
+                int userLife = 100;
+
+                while (true)
+                {
+                    if (cpuLife <= 0 || userLife <= 0)
+                    {
+                        break;
+                    }
+
+                    RoshamboMethods.PrintStatus(cpuLife, userLife);
+
+                    (string cpuThrow, string userThrow) throws = RoshamboMethods.GetThrows();
+
+                    RoshamboMethods.PrintUpdatedScores(ref cpuLife, ref userLife, throws);
+                }
+
+                RoshamboMethods.PrintFinalScores(cpuLife, userLife);
+
+                goAgain = RoshamboMethods.GoAgain();
+
+            } while (goAgain);
+
+            Console.WriteLine("Thanks for Playing - Have a nice day");
+            Console.Write("Press any key to exit. . .");
+            Console.ReadLine();
         }
     }
 }
