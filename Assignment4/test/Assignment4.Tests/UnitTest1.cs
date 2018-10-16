1654a1897729c16ac606d43ff0448f836be4b9fe
Raymond Shiner <raymondshiner@gmail.com>
2018-10-16 10:24:04 -0700

    Assignment4 init - 10/16 10:23am - Laptop

---
+++ Assignment4/test/Assignment4.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Assignment4.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
