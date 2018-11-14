dc18fb5c6cf969ff903737ebfc93151b5747a043
Raymond Shiner <raymondshiner@gmail.com>
2018-11-13 21:22:28 -0800

    Assignment8 init

---
+++ Assignment8/test/Assignment8.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Assignment8.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
